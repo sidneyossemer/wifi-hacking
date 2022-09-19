e56a5da67344460ea5699f7ad37f6a3651a3eb49
Sidney Oss Emer <sidney.ossemer@gmail.com>
2022-09-18 23:48:50 -0300

    Updated combinations set

--- wordlist-personalizada-wh.py
+++ wordlist-personalizada-wh.py
@@ -6,6 +6,7 @@ import datetime
 import re
 from unicodedata import normalize
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-w", "--input", help="Base words")
@@ -26,50 +27,101 @@ if (args.nRange == None):
 	args.nRange = int(1000)
 
 swaps = {'aaeil':'4@311', 'uwm':'vmw', 'ogtssz':'067$52', '12334567890':'izebasgtbgo'}
+chars = {'!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '/', '.', '|', '\\', '?', ',', ';', ':', '<', '>'}
 
 wordlist = open(args.output, 'w')
 date = datetime.datetime.now()
 
-def function_2 (word):
-	for l in range(int(args.nRange) + 1):
-		wordlist.write(word+str(l)+'\n')
-		wordlist.write(word+'@'+str(l)+'\n')
+def removeDuplicates():
+	lines_seen = set()
+	cleanFile = open("clean.txt", "w")
 
-def function_1 (line):
+	for line in open(str(args.output), "r"):
+		if line not in lines_seen:
+			cleanFile.write(line)
+			lines_seen.add(line)
+	cleanFile.close()
+
+def combinations (word):
+	for l in range(int(args.nRange) + 1):
+            wordlist.write(word+str(l)+'\n')
+            wordlist.write(str(l)+word+'\n')
+            for c in chars:
+                wordlist.write(word+c+'\n')
+                wordlist.write(c+word+'\n')
+                if (int(args.complexity) > 0):
+                    wordlist.write(word+c+str(l)+'\n')
+                    wordlist.write(word+str(l)+c+'\n')
+                    wordlist.write(c+word+str(l)+'\n')
+                    wordlist.write(c+str(l)+word+'\n')
+                    wordlist.write(str(l)+word+c+'\n')
+                    wordlist.write(str(l)+c+word+'\n')
+                    if (int(args.complexity) > 1):
+                        for c2 in chars:
+                            wordlist.write(c+word+c2+'\n')
+                            wordlist.write(c+word+c2+str(l)+'\n') 
+                            wordlist.write(c+word+str(l)+c2+'\n')
+                            wordlist.write(c+str(l)+word+c2+'\n') 
+                            wordlist.write(c2+word+c+'\n')
+                            wordlist.write(c2+word+c+str(l)+'\n')
+                            wordlist.write(c2+word+str(l)+c+'\n')
+                            wordlist.write(c2+str(l)+word+c+'\n')
+                        if (int(args.complexity) > 2):
+                            wordlist.write(str(l)+c+word+c2+str(l)+'\n') 
+                            wordlist.write(str(l)+c+word+str(l)+c2+'\n') 
+                            wordlist.write(str(l)+c2+word+c+str(l)+'\n')
+                            wordlist.write(str(l)+c2+word+str(l)+c+'\n')
+                            wordlist.write(c+str(l)+word+c2+str(l)+'\n') 
+                            wordlist.write(c+str(l)+word+str(l)+c2+'\n') 
+                            wordlist.write(c2+str(l)+word+c+str(l)+'\n')
+                            wordlist.write(c2+str(l)+word+str(l)+c+'\n')
+                        if (int(args.complexity) > 3):
+                            for c3 in chars:
+                                wordlist.write(c+str(l)+c2+word+str(l)+c3+'\n')
+                                wordlist.write(c+str(l)+c3+word+str(l)+c2+'\n')
+                                wordlist.write(c+str(l)+c2+word+c3+str(l)+'\n')
+                                wordlist.write(c+str(l)+c3+word+c2+str(l)+'\n')
+                                wordlist.write(c2+str(l)+c+word+str(l)+c3+'\n')
+                                wordlist.write(c2+str(l)+c+word+c3+str(l)+'\n')
+                                wordlist.write(c2+str(l)+c3+word+c+str(l)+'\n')
+                                wordlist.write(c2+str(l)+c3+word+str(l)+c+'\n')
+                                wordlist.write(c3+str(l)+c+word+str(l)+c3+'\n')
+                                wordlist.write(c3+str(l)+c+word+c2+str(l)+'\n')
+                                wordlist.write(c3+str(l)+c2+word+c+str(l)+'\n')
+                                wordlist.write(c3+str(l)+c3+word+str(l)+c+'\n')
+
+def alterCases (line):
 	for i in swaps:
 		wordlist.write(line.translate(str.maketrans(i,swaps[i]))+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])))
+		combinations(line.translate(str.maketrans(i,swaps[i])))
 		wordlist.write(line.translate(str.maketrans(i,swaps[i]))+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i]))+'@'+str(date.year)+'\n')
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).upper()+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])).upper())
+		combinations(line.translate(str.maketrans(i,swaps[i])).upper())
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).upper()+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i])).upper()+'@'+str(date.year)+'\n')
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).lower()+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])).lower())
+		combinations(line.translate(str.maketrans(i,swaps[i])).lower())
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).lower()+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i])).lower()+'@'+str(date.year)+'\n')
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).capitalize()+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])).capitalize())
+		combinations(line.translate(str.maketrans(i,swaps[i])).capitalize())
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).capitalize()+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i])).capitalize()+'@'+str(date.year)+'\n')
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).title()+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])).title())
+		combinations(line.translate(str.maketrans(i,swaps[i])).title())
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).title()+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i])).title()+'@'+str(date.year)+'\n')
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).swapcase()+'\n')
-		function_2(line.translate(str.maketrans(i,swaps[i])).swapcase())
+		combinations(line.translate(str.maketrans(i,swaps[i])).swapcase())
 		wordlist.write(line.translate(str.maketrans(i,swaps[i])).swapcase()+str(date.year)+'\n')
-		wordlist.write(line.translate(str.maketrans(i,swaps[i])).swapcase()+'@'+str(date.year)+'\n')
 
 def main():
 	print( f"{date:%Y-%m-%d %H:%M:%S}"+"\tProcess started")
 	with open(args.input) as file:
 		for t in file:
 			linha = t.rstrip('\n')
-			function_1(linha)
+			alterCases(linha)
+	removeDuplicates()
 	wordlist.close()
+	os.remove(args.output)
+	os.rename('clean.txt', args.output)
 	print( f"{date:%Y-%m-%d %H:%M:%S}"+"\tProcess finished. File "+args.output+" saved.")
 
 if __name__ == '__main__':
-  main()
+	main()
